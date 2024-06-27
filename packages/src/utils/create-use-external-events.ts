cfe6b561b569f3397d37be5abb42fc407381b80a
been.im <been.im@toss.im>
2024-06-27 17:36:31 +0900

    feat: Remove window dom dependencies

--- packages/src/utils/create-use-external-events.ts
+++ packages/src/utils/create-use-external-events.ts
@@ -1,35 +1,37 @@
+import mitt from 'mitt';
 import { useEffect, useLayoutEffect } from 'react';
 
+const emitter = mitt();
 const useIsomorphicEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect;
 
 function dispatchEvent<Detail>(type: string, detail?: Detail) {
-  window.dispatchEvent(new CustomEvent(type, { detail }));
+  emitter.emit(type, detail);
 }
 
 // event를 생성할 때 params는 어떤 타입이든 올 수 있기 때문에 any로 지정합니다.
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function createUseExternalEvents<EventHandlers extends Record<string, (params: any) => void>>(prefix: string) {
   function useExternalEvents(events: EventHandlers) {
-    const handlers = Object.keys(events).reduce<Record<string, EventListener>>((prev, eventKey) => {
+    const handlers = Object.keys(events).reduce<Record<string, () => void>>((prev, eventKey) => {
       const currentEventKeys = `${prefix}:${eventKey}`;
 
       return {
         ...prev,
-        [currentEventKeys]: function (event: CustomEvent) {
-          events[eventKey](event.detail);
-        } as EventListener,
+        [currentEventKeys]: function (event: unknown) {
+          events[eventKey](event);
+        } as () => void,
       };
     }, {});
 
     useIsomorphicEffect(() => {
       Object.keys(handlers).forEach((eventKey) => {
-        window.removeEventListener(eventKey, handlers[eventKey]);
-        window.addEventListener(eventKey, handlers[eventKey]);
+        emitter.off(eventKey, handlers[eventKey]);
+        emitter.on(eventKey, handlers[eventKey]);
       });
 
       return () =>
         Object.keys(handlers).forEach((eventKey) => {
-          window.removeEventListener(eventKey, handlers[eventKey]);
+          emitter.off(eventKey, handlers[eventKey]);
         });
     }, [handlers]);
   }

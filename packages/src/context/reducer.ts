3734d72fb183f99c849d44a6bba88b43554a6254
po4tion <po4tion0429@gmail.com>
2024-06-29 01:34:58 +0900

    refactor: apply immer

--- packages/src/context/reducer.ts
+++ packages/src/context/reducer.ts
@@ -1,3 +1,4 @@
+import { produce, type Draft } from 'immer';
 import type { OverlayControllerComponent } from './context';
 
 export type OverlayItem = {
@@ -21,73 +22,44 @@ export type OverlayReducerAction =
   | { type: 'REMOVE_ALL' };
 
 export function overlayReducer(state: OverlayReducerState, action: OverlayReducerAction): OverlayReducerState {
-  switch (action.type) {
-    case 'ADD': {
-      return {
-        current: action.overlay.id,
-        overlayOrderList: [...state.overlayOrderList, action.overlay.id],
-        overlayData: {
-          ...state.overlayData,
-          [action.overlay.id]: action.overlay,
-        },
-      };
-    }
-    case 'OPEN': {
-      return {
-        ...state,
-        overlayData: {
-          ...state.overlayData,
-          [action.overlayId]: {
-            ...state.overlayData[action.overlayId],
-            isOpen: true,
-          },
-        },
-      };
-    }
-    case 'CLOSE': {
-      return {
-        ...state,
-        overlayData: {
-          ...state.overlayData,
-          [action.overlayId]: {
-            ...state.overlayData[action.overlayId],
-            isOpen: false,
-          },
-        },
-      };
-    }
-    case 'REMOVE': {
-      const remainingOverlays = state.overlayOrderList.filter((item) => item !== action.overlayId);
-      if (state.overlayOrderList.length === remainingOverlays.length) {
-        return state;
+  return produce(state, (draft: Draft<OverlayReducerState>) => {
+    switch (action.type) {
+      case 'ADD': {
+        draft.current = action.overlay.id;
+        draft.overlayOrderList.push(action.overlay.id);
+        draft.overlayData[action.overlay.id] = action.overlay;
+        break;
       }
+      case 'OPEN': {
+        draft.overlayData[action.overlayId].isOpen = true;
+        break;
+      }
+      case 'CLOSE': {
+        draft.overlayData[action.overlayId].isOpen = false;
+        break;
+      }
+      case 'REMOVE': {
+        const remainingOverlays = draft.overlayOrderList.filter((item) => item !== action.overlayId);
 
-      const copiedOverlayData = { ...state.overlayData };
-      delete copiedOverlayData[action.overlayId];
+        if (state.overlayOrderList.length === remainingOverlays.length) {
+          break;
+        }
 
-      return {
-        current: remainingOverlays.at(-1) || state.current,
-        overlayOrderList: remainingOverlays,
-        overlayData: copiedOverlayData,
-      };
+        draft.overlayOrderList = remainingOverlays;
+        delete draft.overlayData[action.overlayId];
+        draft.current = remainingOverlays.at(-1) || state.current;
+        break;
+      }
+      case 'CLOSE_ALL': {
+        Object.keys(draft.overlayData).forEach((key) => (draft.overlayData[key].isOpen = false));
+        break;
+      }
+      case 'REMOVE_ALL': {
+        draft.overlayOrderList = [];
+        draft.overlayData = {};
+        draft.current = state.current;
+        break;
+      }
     }
-    case 'CLOSE_ALL': {
-      return {
-        ...state,
-        overlayData: Object.keys(state.overlayData).reduce(
-          (prev, curr) => ({
-            ...prev,
-            [curr]: {
-              ...state.overlayData[curr],
-              isOpen: false,
-            } satisfies OverlayItem,
-          }),
-          {} satisfies Record<string, OverlayItem>
-        ),
-      };
-    }
-    case 'REMOVE_ALL': {
-      return { current: state.current, overlayOrderList: [], overlayData: {} };
-    }
-  }
+  });
 }

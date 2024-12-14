2fa322c2e2b6c28e68d70ff8b7b0f1174b099fba
been.im <meis1541@naver.com>
2024-12-14 13:55:10 +0900

    feat(docs): Add typscript configuration

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -2,7 +2,7 @@ import nextra from 'nextra';
 
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
+  themeConfig: './theme.config.tsx',
 });
 
 export default withNextra();

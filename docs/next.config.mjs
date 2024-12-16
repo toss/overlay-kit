f3bb6690c5967b8f4c3992a255c2298da4b863d1
been.im <meis1541@naver.com>
2024-12-17 00:43:52 +0900

    chore(docs): Add remark-sandpack for enhanced MDX support

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,9 +1,13 @@
 import stylexPlugin from '@stylexswc/nextjs-plugin';
 import nextra from 'nextra';
+import { remarkSandpack } from 'remark-sandpack';
 
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  mdxOptions: {
+    remarkPlugins: [remarkSandpack],
+  },
 });
 
 export default stylexPlugin({

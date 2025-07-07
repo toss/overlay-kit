4fabdb4175404f924cdb1a88522ffbc3f67d814b
been.im <meis1541@naver.com>
2025-07-07 16:43:26 +0900

    fix: Resolve issue with some overlays not opening

--- packages/src/context/provider/index.tsx
+++ packages/src/context/provider/index.tsx
@@ -44,12 +44,39 @@ export function createOverlayProvider() {
 
     useOverlayEvent({ open, close, unmount, closeAll, unmountAll });
 
+    /**
+     * @TODO
+     * Due to React batching, only the current of the last modal can be recognized,
+     * so when n modals closed by close are opened at once with overlay.open,
+     * some modals may not open.
+     *
+     * To solve this, prevOverlayState.current.overlayData's isOpen should be false,
+     * and overlayState.overlayData's isOpen should be true.
+     * Reducer modification is needed to handle this part, and since this issue
+     * has existed from before, we'll skip it for now.
+     */
     if (prevOverlayState.current !== overlayState) {
-      prevOverlayState.current = overlayState;
+      // mounted overlay open
+      overlayState.overlayOrderList.forEach((overlayId) => {
+        if (prevOverlayState.current.overlayData[overlayId] == null) {
+          requestAnimationFrame(() => {
+            overlayDispatch({ type: 'OPEN', overlayId });
+          });
+        }
+      });
 
-      if (overlayState.current != null && overlayState.overlayData[overlayState.current].isOpen === false) {
-        overlayDispatch({ type: 'OPEN', overlayId: overlayState.current });
+      // reopened overlay open
+      if (overlayState.current != null && prevOverlayState.current.overlayData[overlayState.current] != null) {
+        const isPrevOverlayClosed = prevOverlayState.current.overlayData[overlayState.current].isOpen === false;
+
+        if (isPrevOverlayClosed) {
+          requestAnimationFrame(() => {
+            overlayDispatch({ type: 'OPEN', overlayId: overlayState.current! });
+          });
+        }
       }
+
+      prevOverlayState.current = overlayState;
     }
 
     useEffect(() => {
